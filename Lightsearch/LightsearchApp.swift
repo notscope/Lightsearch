ac86e5a10a146f1511ce263e93b171cc3ee63591
notscope <noreply@github.com>
2026-09-02 17:19:52 +0700

    Initial Commit

---
+++ Lightsearch/LightsearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightsearchApp.swift
+//  Lightsearch
+//
+//  Created by Lightsearch on 02/09/26.
+//
+
+import SwiftUI
+
+@main
+struct LightsearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
